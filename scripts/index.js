0d6aadf321ace946c1de085db8689d3f8df98710
Najot Nafasov <nigelnafasov98@yandex.com>
2025-03-12 00:48:10 +0500

    JS. добавление карточек и кнопки удаления

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,34 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+
+function createCard(cardData, deleteCard) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+    const cardImage = cardElement.querySelector('.card__image');
+    const cardTitle = cardElement.querySelector('.card__title');
+    
+    cardImage.src = cardData.link;
+    cardImage.alt = cardData.name;
+    cardTitle.textContent = cardData.name;
+
+    const cardDeleteButton = cardElement.querySelector('.card__delete-button');
+    cardDeleteButton.addEventListener('click', () => {
+        deleteCard(cardElement);
+    });
+
+    return cardElement;
+}
+
+function deleteCard(cardElement) {
+    cardElement.remove();
+}
+
+const placesList = document.querySelector('.places__list');
+
+initialCards.forEach(cardData => {
+    const cardElement = createCard(cardData, deleteCard);
+    placesList.appendChild(cardElement);
+})
